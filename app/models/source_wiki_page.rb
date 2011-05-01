46b1c6a0e349c2cf5a67a9e36daab9cdbb2be13b
Mohammad Ebrahim Mohammadi Panah <ebrahim@mohammadi.ir>
2011-05-01 21:48:37 +0430

    Fixed migration of wiki pages with deep hierarchies using a recursive solution

--- app/models/source_wiki_page.rb
+++ app/models/source_wiki_page.rb
@@ -3,14 +3,25 @@ class SourceWikiPage < ActiveRecord::Base
   set_table_name :wiki_pages
 
   def self.migrate
-    all(:order => 'parent_id ASC').each do |source_wiki_page|
-
-      wiki_page = WikiPage.create!(source_wiki_page.attributes) do |wp|
-        wp.wiki = Wiki.find(RedmineMerge::Mapper.get_new_wiki_id(source_wiki_page.wiki_id))
-        wp.parent = WikiPage.find(RedmineMerge::Mapper.get_new_wiki_page_id(source_wiki_page.parent_id)) if source_wiki_page.parent_id
-      end
-
-      RedmineMerge::Mapper.add_wiki_page(source_wiki_page.id, wiki_page.id)
+    all.each do |source_wiki_page|
+      recursive_migrate source_wiki_page.id
     end
+  end
+
+  def self.recursive_migrate(source_wiki_page_id)
+    # Return if already migrated
+    if new_id = RedmineMerge::Mapper.get_new_wiki_page_id(source_wiki_page_id)
+		return new_id
+	end
+
+    source_wiki_page = SourceWikiPage.find(source_wiki_page_id)
+    wiki_page = WikiPage.create(source_wiki_page.attributes)
+    wiki_page.wiki = Wiki.find(RedmineMerge::Mapper.get_new_wiki_id(source_wiki_page.wiki_id))
+    wiki_page.parent = WikiPage.find(recursive_migrate(source_wiki_page.parent_id)) if source_wiki_page.parent_id?
+    wiki_page.save()
+
+    RedmineMerge::Mapper.add_wiki_page(source_wiki_page.id, wiki_page.id)
+
+    return wiki_page.id
   end
 end
